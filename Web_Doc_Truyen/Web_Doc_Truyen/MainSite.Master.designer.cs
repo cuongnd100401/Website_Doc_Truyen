ad96107823dc4de1a5e5b4469fb7c88583cffcc5
cuongnd100401 <nguyenduccuong100401@gmail.com>
2022-05-21 21:18:59 +0700

    2nd commit

--- Web_Doc_Truyen/Web_Doc_Truyen/MainSite.Master.designer.cs
+++ Web_Doc_Truyen/Web_Doc_Truyen/MainSite.Master.designer.cs
@@ -33,6 +33,24 @@ namespace Web_Doc_Truyen
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
 
         /// <summary>
+        /// DLDM control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DataList DLDM;
+
+        /// <summary>
+        /// DLTL control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DataList DLTL;
+
+        /// <summary>
         /// ContentPlaceHolder1 control.
         /// </summary>
         /// <remarks>
