4ecf5ea5667bfa7f714f10ffc9d9741e8dd0fa1f
cuongnd100401 <nguyenduccuong100401@gmail.com>
2022-05-22 15:11:47 +0700

    3rd commit

--- Web_Doc_Truyen/Web_Doc_Truyen/HomePage.aspx
+++ Web_Doc_Truyen/Web_Doc_Truyen/HomePage.aspx
@@ -2,4 +2,22 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div class="HomePage">
+        <div class="TieuDe">
+            TRUYỆN HOT
+            <i class="fa-brands fa-hotjar marginl"></i>
+        </div>
+        <div class="DSTuyen">
+            <asp:DataList ID="DLTH" runat="server" RepeatColumns="5" CssClass="">
+                <ItemTemplate>
+                    <a href="#">
+                        <div class="Truyen-it">
+                            <img src="<%#Eval("HinhAnh") %>" style="width: 170px; height: 226px;"/>
+                            <p class="tenTruyen"><%#Eval("TenT") %></p>
+                        </div>
+                    </a>
+                </ItemTemplate>
+            </asp:DataList>
+        </div>
+    </div>
 </asp:Content>
--- Web_Doc_Truyen/Web_Doc_Truyen/HomePage.aspx.cs
+++ Web_Doc_Truyen/Web_Doc_Truyen/HomePage.aspx.cs
@@ -4,14 +4,32 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace Web_Doc_Truyen
 {
     public partial class HomePage : System.Web.UI.Page
     {
+        string str = @"Data Source=DESKTOP-FA5AISU\SQLEXPRESS;Initial Catalog=WebDocTruyen;Integrated Security=True";
+        SqlConnection con;
+        SqlCommand com = new SqlCommand();
+        SqlDataAdapter dad = new SqlDataAdapter();
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            con = new SqlConnection(str);
+            con.Open();
+            loadData();
+        }
+        public void loadData()
+        {
+            com = con.CreateCommand();
+            com.CommandText = "select * from tblTruyen";
+            dad.SelectCommand = com;
+            DataSet ds = new DataSet();
+            dad.Fill(ds);
+            DLTH.DataSource = ds;
+            DLTH.DataBind();
         }
     }
 }
